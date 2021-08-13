d61240e193080e4640ef6bd0d207f63d5572f3d2
Luis Manuel Alvarez Bautista <lmalvarez@personalsoft.com.co>
2021-08-12 21:35:28 -0500

    eslint

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import HelloWord from './components/HelloWorld'
+
+ReactDOM.render(
+  <HelloWord />,
+  document.getElementById('app')
+);
